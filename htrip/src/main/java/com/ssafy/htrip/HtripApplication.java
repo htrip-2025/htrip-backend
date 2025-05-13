5d61b6b0a9ae41f64fc046f9194af7e744115f6d
YoungH098 <dudgh3796@naver.com>
2025-05-13 16:44:21 +0900

    컨트롤러

---
+++ htrip/src/main/java/com/ssafy/htrip/HtripApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.htrip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HtripApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HtripApplication.class, args);
+	}
+
+}
---
+++ htrip/src/test/java/com/ssafy/htrip/HtripApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.htrip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HtripApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
