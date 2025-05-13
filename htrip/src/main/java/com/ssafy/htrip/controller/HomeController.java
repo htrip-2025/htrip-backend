9345bcdd00037f91b1ea2bf968dfa0007c4f364a
YoungH098 <dudgh3796@naver.com>
2025-05-13 16:41:05 +0900

    컨트롤러

---
+++ htrip/src/main/java/com/ssafy/htrip/controller/HomeController.java
@@ -0,0 +1,21 @@
+package com.ssafy.htrip.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Slf4j
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() throws Exception{
+        return "index";
+    }
+
+    @GetMapping("/index")
+    public String index() throws Exception{
+        return "index";
+    }
+
+}
