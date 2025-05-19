49127be284291f91d826b246b165c61373ba1ce2
YoungH098 <dudgh3796@naver.com>
2025-05-19 12:20:08 +0900

    localhost:5174로 주소변경

--- htrip/src/main/java/com/ssafy/htrip/auth/handler/CustomSuccessHandler.java
+++ htrip/src/main/java/com/ssafy/htrip/auth/handler/CustomSuccessHandler.java
@@ -42,7 +42,7 @@ public class CustomSuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
         String token = jwtUtil.createJwt(userId,username, Role.valueOf(role), 1000*60*60L);
 
         response.addCookie(createCookie("Authorization", token));
-        response.sendRedirect("http://localhost:3000/");
+        response.sendRedirect("http://localhost:5174/");
     }
 
     private Cookie createCookie(String key, String value) {
--- htrip/src/main/java/com/ssafy/htrip/common/config/SecurityConfig.java
+++ htrip/src/main/java/com/ssafy/htrip/common/config/SecurityConfig.java
@@ -52,7 +52,7 @@ public class SecurityConfig {
 
                         CorsConfiguration configuration = new CorsConfiguration();
 
-                        configuration.setAllowedOrigins(Collections.singletonList("http://localhost:3000")); //뷰에 맞춰서 변경
+                        configuration.setAllowedOrigins(Collections.singletonList("http://localhost:5174")); //뷰에 맞춰서 변경
                         configuration.setAllowedMethods(Collections.singletonList("*"));
                         configuration.setAllowCredentials(true);
                         configuration.setAllowedHeaders(Collections.singletonList("*"));
