574408b6edae8efe40024f8c8b0c4056fb5d1ebe
hwi-lee <riteru66@naver.com>
2025-05-24 21:25:17 +0900

    Update BoardCategoryRepository.java

--- htrip/src/main/java/com/ssafy/htrip/board/repository/BoardCategoryRepository.java
+++ htrip/src/main/java/com/ssafy/htrip/board/repository/BoardCategoryRepository.java
@@ -5,4 +5,6 @@ import com.ssafy.htrip.board.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BoardCategoryRepository extends JpaRepository<Category, Integer> {
+    // 카테고리명으로 검색
+    Category findByCategoryName(String categoryName);
 }
